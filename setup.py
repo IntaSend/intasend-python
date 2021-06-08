48e38639d96562ea21ccd806ee28881c1ac2e679
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2021-06-08 11:16:16 +0300

    Add setup files

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+    name='intasend-python',
+    packages=['intasend-python'],
+    version='0.1',
+    license='MIT',
+    description='Official Python SDK for IntaSend Payments Gateway API',
+    author='Felix Cheruiyot',
+    author_email='support@intasend.com',
+    url='https://github.com/IntaSend/intasend-python',
+    download_url='https://github.com/IntaSend/intasend-python/archive/v_01.tar.gz',
+    keywords=['payments', 'mpesa', 'card payments',
+              'payments kenya', 'intasend'],
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
