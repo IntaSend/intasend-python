c149a627f3374bafefaa83ccb7911ed96b9d58bd
Felix Cheruiyot <felix.cheruiyot@kenyaapps.net>
2022-01-25 08:38:31 +0300

    Update version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='intasend-python',
     packages=['intasend'],
-    version='1.0.1',
+    version='1.0.2',
     license='MIT',
     description='Official Python SDK for IntaSend Payments Gateway API',
     author='Felix Cheruiyot',
     author_email='support@intasend.com',
     url='https://github.com/IntaSend/intasend-python',
-    download_url='https://github.com/IntaSend/intasend-python/archive/v_1.0.1.tar.gz',
+    download_url='https://github.com/IntaSend/intasend-python/archive/v_1.0.2.tar.gz',
     keywords=['payments', 'mpesa', 'card payments', 'visa',
               'mastercard', 'payments kenya', 'intasend'],
     install_requires=[
