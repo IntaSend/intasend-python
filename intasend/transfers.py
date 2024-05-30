bf9b160ee797619092de385d5cd01774f4bd6e74
Mugendi Gitonga <gitongav97@gmail.com>
2024-05-30 10:59:06 +0300

    approval flag added to airtime

--- intasend/transfers.py
+++ intasend/transfers.py
@@ -40,6 +40,6 @@ class Transfer(APIBase):
     def get_bank_codes(self):
         return self.send_request("GET", "send-money/bank-codes/ke/", {}, noauth=True)
     
-    def airtime(self, currency="KES", transactions=[], callback_url=None, wallet_id=None):
+    def airtime(self, currency="KES", transactions=[], requires_approval="YES", callback_url=None, wallet_id=None):
         provider = "AIRTIME"
-        return self.send_money(provider, currency, transactions, callback_url, wallet_id)
+        return self.send_money(provider, currency, transactions, callback_url, wallet_id, requires_approval)
