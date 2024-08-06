44155a377d14a89a3f4806d96cd7e309d622b125
FELIX CHERUIYOT <felix@intasend.com>
2024-08-06 01:38:46 +0000

    Changed version

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name='intasend-python',
-    packages=['intasend'],
-    version='1.1.0',
+    packages=['intasend', ],
+    version='1.1.2',
     license='MIT',
     description='Official Python SDK for IntaSend Payments Gateway API',
     readme='README.md',
     author='Felix Cheruiyot',
     author_email='support@intasend.com',
     url='https://github.com/IntaSend/intasend-python',
-    download_url='https://github.com/IntaSend/intasend-python/archive/v_1.0.8.tar.gz',
+    # download_url='https://github.com/IntaSend/intasend-python/archive/v_1.1.2.tar.gz',
     keywords=['payments', 'mpesa', 'mpesa api', 'card payments', 'visa',
               'mastercard', 'payments kenya', 'mpesa stk-push', 'stk push', 'intasend', 'airtime',],
     install_requires=[
